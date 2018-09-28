d239cce5dc4d0c23fdc81062b3d07c6a2077dc8e
Ilian Ivanov <noodle.hell@gmail.com>
2018-09-28 16:43:27 +0300

    asd

--- borderify.js
+++ borderify.js
@@ -1,27 +1,40 @@
 
-
-var element = document.getElementsByClassName('nexxt')[0];
+console.log("START")
+// TITLE
 var title = document.getElementsByTagName('title')[0].innerHTML
-var navig = document.getElementsByClassName('vjs-control-bar')[0]
-
 var res = title.replace("Watch","")
 document.getElementsByTagName('title')[0].innerHTML = res
-// console.log("naig", playbtn)
+
+
+
+// USE Z KEY FOR NEXT EP
+var element = document.getElementsByClassName('nexxt')[0];
 document.onkeydown = function(e){
-    e = e || window.event;
-    var key = e.which || e.keyCode;
+  e = e || window.event;
+  var key = e.which || e.keyCode;
     if(key===90){ // "Z" key
-      element.click();
-    }
+    element.click();
+  }
+}
+// new button in video controller
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    var el = document.createElement('div');
+    el.innerHTML = '<button class="nextM">HI</button>'
+    console.log(el.firstChild)
+    console.log("STATE", document.readyState)
+    var playbtn = document.getElementsByClassName('vjs-big-play-button')[0]
+    playbtn.addEventListener('click', function(){
+      console.log("CLICKED--------------------")
+      var control = document.getElementsByClassName('vjs-control-bar')[0]
+      var nextClip = control.appendChild(el.firstChild)
+      console.log(control)
+      console.log(nextClip)
+      nextClip.addEventListener('click', function(){
+        element.click()
+      })
+    })
+
+  }
 }
 
-setTimeout(function(){
-  var playbtn = document.getElementsByClassName('vjs-big-play-button')[0]
-
-  console.log(playbtn)
-}, 10000)
-  var navig = document.getElementsByClassName('vjs-control-bar')[0]
-playbtn.addEventListener('click', function(){
-  console.log('asd')
-})
-console.log(playbtn, "playbtdjsj")
